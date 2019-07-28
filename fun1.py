b161003b888c7deae95e7f39dfbecaf01d2df142
eviatar21-meet <eviatar21@meet.mit.edu>
2019-07-28 16:32:01 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,11 @@
+def add_numbers(start, end):
+    for number in range(start, end+1):
+        a= number + 1
+    return (a)
+
+test1 = add_numbers(1,2)
+print(test1)
+test2 = add_numbers(1, 100)
+print(test2)
+test3 = add_numbers(1000, 5000)
+print(test3)
